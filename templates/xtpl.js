fb6b6a0d199b39611f94e9fbc98c7be820e63b6c
leolems <leo@2basix.nl>
2014-08-04 15:10:37 +0200

    Update xtpl.js

--- templates/xtpl.js
+++ templates/xtpl.js
@@ -3,49 +3,10 @@
 //   Micro JavaScript Templating   xtpl (minified version)
 //   Leo Lems - http://2basix.nl 
 // 		original code by John Resig - MIT Licensed
+//
+//	This is the xtpl string addition variant (which was the fastest version of xtpl)
+//	the JS code itself is only 681 bytes !!
+//
+// Here comes the magical function that will power your web-application (lean and mean) xtpl+=
 ------------------------------------------------------------*/
-// Here comes the magical function that will power your web-application
-(function(){
-
-	this.xtpl = function xtpl(templatestring, data){
-		//======================================================================
-		//==-- 2basix version  (improved code..)	
-		//======================================================================
-		var ret='', err='';
-		
-		// Generate a reusable function that will serve as a template generator.
-		try{ 
-				var rdata = /{%=\s*(.+?)\s*%}/g, 
-					rfunc = /{%=\s*\(\s*(.+?)\s*\)\s*%}/g,
-					pre_tpl =	"var arr=[]; arr.push('"+
-								templatestring	.replace(/[\r\t\v\n\b]/g, "")
-												.split("'").join("\\'")
-												.split("{%js").join("');")
-												.split("js%}").join("; arr.push('")
-								+"'); return arr.join('');",
-											
-					xfn =   // Generate a reusable function that will serve as a template generator.
-							new Function("xtplData", 
-									pre_tpl.replace(rfunc, "',(xtplData.$1),'")		// take care off functions
-										   .replace(rdata, "',(xtplData.$1)?xtplData.$1:'','")
-							);
-
-		} catch(err) { 
-				txt="xtpl: There is an error inside the template string\n\n";
-				txt+="Click OK to continue.\n\n";
-				alert(txt); 
-				return '';
-		}
-		if (!data) { return xfn }	// return the function !!!
-
-		//=================================================
-		try{ ret=xfn(data); } catch(err) { 
-				txt="xtpl: Error occured when generating the HTML from the template \n\n";
-				txt+="Click OK to continue.\n\n";
-				alert(txt); 
-				return '';
-		}
- 		return ret;
-	};
-})();
-
+(function(){this.xtpl_add=function(t,n){var r="",i="";try{var s=/{%=\s*(.+?)\s*%}/g,o=/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,u="var p='"+t.replace(/[\r\t\v\n\b]/g,"").split("'").join("\\'").split("{%js").join("';").split("js%}").join("; p+='")+"'; return p;";xfn=new Function("xtplData",u.replace(o,"'; p+=(xtplData.$1); p+='").replace(s,"'; p+=((xtplData.$1)?xtplData.$1:''); p+='"))}catch(i){txt="xtpl: There is an error inside the template string\n\n";txt+="Click OK to continue.\n\n";alert(txt);return""}if(!n){return xfn}try{r=xfn(n)}catch(i){txt="xtpl: Error occured when generating the HTML from the template \n\n";txt+="Click OK to continue.\n\n";alert(txt);return""}return r}})();
