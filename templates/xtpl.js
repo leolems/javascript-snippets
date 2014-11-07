3e1b144b032c35a6e7867ffe2d808e6a05b301a4
leolems <leo@2basix.nl>
2014-11-07 11:27:18 +0100

    minor changes

--- templates/xtpl.js
+++ templates/xtpl.js
@@ -1,17 +1,10 @@
 /*----------------------------------------------------------
-//   Micro JavaScript Templating   xtpl (minified version)
-//   Leo Lems - http://2basix.nl - MIT Licensed
-------------------------------------------------------------*/
-/*  old version 
-(function(){this.xtpl=function xtpl(templatestring,data){var ret='',xfn=new Function("o","var arr=['"+templatestring.replace(/[\r\t\n]/g,"").split("'").join("\\'").replace(/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,"',($1),'").replace(/{%=\s*(.+?)\s*%}/g,"',(o.$1)?o.$1:'','")+"']; return arr.join('');");if(!data){return xfn}return xfn(data)}})();
-*/
-
-/*----------------------------------------------------------
-//   Micro JavaScript Templating   xtpl (minified version)
+//   Micro JavaScript Templating   xtpl (minified version += version)
 //   Leo Lems - http://2basix.nl 
-// 		original code by John Resig - MIT Licensed
-// 		723 bytes
+// 		note: Original JS code by John Resig - MIT Licensed
+// 		xtpl += version 
+//			size: 744 bytes of code
+//			author: L.Lems
 ------------------------------------------------------------*/
-(function(){this.xtpl=function xtpl(a,b){var c='',err='';try{var d=/{%=\s*(.+?)\s*%}/g,rfunc=/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,pre_tpl="var arr=[]; arr.push('"+a.replace(/[\r\t\v\n\b]/g,"").split("'").join("\\'").split("{%js").join("');").split("js%}").join("; arr.push('")+"'); return arr.join('');",xfn=new Function("xtplData",pre_tpl.replace(rfunc,"',(xtplData.$1),'").replace(d,"',(xtplData.$1)?xtplData.$1:'','"))}catch(err){txt="xtpl: There is an error inside the template string\n\n";txt+="Click OK to continue.\n\n";alert(txt);return''}if(!b){return xfn}try{c=xfn(b)}catch(err){txt="xtpl: Error occured when generating the HTML from the template \n\n";txt+="Click OK to continue.\n\n";alert(txt);return''}return c}})();
-
+(function(){this.xtpl=function(t,n){var r="",i="",s="";try{var o=/{%=\s*(.+?)\s*%}/g,u=/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,a="var p='"+t.replace(/[\r\t\v\n\b]/g,"").split("'").join("\\'").split("{%js").join("';").split("js%}").join("; p+='")+"'; return p;";var f=new Function("xtplData",a.replace(u,"'; p+=(xtplData.$1); p+='").replace(o,"'; p+=((xtplData.$1)?xtplData.$1:''); p+='"))}catch(i){s="xtpl: There is an error inside the template string.";if(window.console&&window.console.log){console.log(s);console.log("Template: "+t)}return""}if(!n){return f}try{r=f(n)}catch(i){s="xtpl: Error occured when generating the HTML from the template.";if(window.console&&window.console.log){console.log(s);console.log("Template: "+t)}return""}return r}})();
 
