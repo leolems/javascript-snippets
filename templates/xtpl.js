52fad0271779ea4cd0ba8d20f6a7e93efd1658be
Leo Lems <leo@2basix.nl>
2015-01-12 14:45:29 +0100

    Update xtpl.js

--- templates/xtpl.js
+++ templates/xtpl.js
@@ -7,4 +7,13 @@
 // 		xtpl += version 
 //			size: 1364 bytes of code
 ---------------------------------------------------------------------------*/
-String.prototype.arrsplit||(String.prototype.arrsplit=function(t){var e,r,o,n,l,s=[],i=[];for(s[0]=this,o=t&&!t.propertyIsEnumerable("length")&&"object"==typeof t&&"number"==typeof t.length?t.length:1,e=0;o>e;e++){for(i=[],n=s.length,r=0;n>r;r++)l=s[r].split(t[e]),i=i.concat(l);s=i}return s}),function(){this.xtpl=function(t,e){var r="",o="";try{var n=/{%=\s*(.+?)\s*%}/g,l=/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,s=t.replace(/[\r\t\v\n\b]/g,"");if(t.search("{%if}")>-1&&(s=s.split("{%if}").join("{%js if(").split("{if%}").join("){ js%}").split("{%endif%}").join("{%js } js%}").split("{%else%}").join("{%js } else { js%}")),s.search("{%js")>-1){s=s.arrsplit(["{%js","js%}"]);var i,p;for(p=s.length,i=0;p>i;i++)i%2&&(s[i]="{%js"+s[i].replace(n,"xtplData.$1")+"js%}");s=s.join("")}var a="var p='"+s.split("'").join("\\'").split("{%js").join("';").split("js%}").join("; p+='")+"'; return p;",c=Function("xtplData",a.replace(l,"'; p+=(xtplData.$1); p+='").replace(n,"'; p+=((xtplData.$1)?xtplData.$1:''); p+='"))}catch(j){return o="xtpl: There is an error inside the template string.",window.console&&window.console.log&&(console.log(o),console.log("Template: "+t)),""}if(!e)return c;try{r=c(e)}catch(j){return o="xtpl: Error occured when generating the HTML from the template.",window.console&&window.console.log&&(console.log(o),console.log("Template: "+t)),""}return r}}();
+//	split a string using an array (to split up by using multiple arguments)
+String.prototype.arrsplit||(String.prototype.arrsplit=function(t){var e,o,r,n,l,s=[],i=[]
+for(s[0]=this,r=t&&!t.propertyIsEnumerable("length")&&"object"==typeof t&&"number"==typeof t.length?t.length:1,e=0;r>e;e++){for(i=[],n=s.length,o=0;n>o;o++)l=s[o].split(t[e]),i=i.concat(l)
+s=i}return s}),function(){this.xtpl=function(t,e){var o="",r=""
+try{var n=/{%=\s*(.+?)\s*%}/g,l=/{%=\s*\(\s*(.+?)\s*\)\s*%}/g,s=t.replace(/[\r\t\v\n\b]/g,"")
+if(t.search("{%if}")>-1&&(s=s.split("{%if}").join("{%js if(").split("{if%}").join("){ js%}").split("{%endif%}").join("{%js } js%}").split("{%else%}").join("{%js } else { js%}")),s.search("{%js")>-1){s=s.arrsplit(["{%js","js%}"])
+var i,p
+for(p=s.length,i=0;p>i;i++)i%2&&(s[i]="{%js"+s[i].replace(n,"xtplData.$1")+"js%}")
+s=s.join("")}var a="var p='"+s.split("'").join("\\'").split("{%js").join("';").split("js%}").join("p+='")+"'; return p;",c=Function("xtplData",a.replace(l,"'+(xtplData.$1)+'").replace(n,"'+((xtplData.$1)?xtplData.$1:'')+'"))}catch(j){return r="xtpl: There is an error inside the template string.",window.console&&window.console.log&&(console.log(r),console.log("Template: "+t)),""}if(!e)return c
+try{o=c(e)}catch(j){return r="xtpl: Error occured when generating the HTML from the template.",window.console&&window.console.log&&(console.log(r),console.log("Template: "+t)),""}return o}}();
